f55cbaf6f9dd96d1efff114df693d7d9cf7e5355
Tuo Nome <ambra_canale@libero.it>
2025-05-05 11:42:06 +0200

    prova git

---
+++ prova.py
@@ -0,0 +1,6 @@
+#fai una domanda
+domanda = input("inserisci una domanda: ")
+#fai una risposta
+risposta = "la risposta è 42"
+#stampo la domanda e la risposta
+print(f"domanda: {domanda}\nrisposta: {risposta}")
---
+++ prova2.py
@@ -0,0 +1,7 @@
+#chiedi due numeri
+num1 = int(input("Inserisci il primo numero: "))
+num2 = int(input("Inserisci il secondo numero: "))
+#fai la somma
+somma = num1 + num2
+#stampa la somma
+print("La somma dei due numeri è: ", somma)
